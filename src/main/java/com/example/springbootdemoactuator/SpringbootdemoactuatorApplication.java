0c56260f030764a8a9d3ccfdd270f0cd4557dfd2
jiangfeilong123 <2962735074@qq.com>
2019-11-28 13:41:25 +0800

    first commit

---
+++ src/main/java/com/example/springbootdemoactuator/SpringbootdemoactuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemoactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoactuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoactuatorApplication.class, args);
+    }
+
+}
